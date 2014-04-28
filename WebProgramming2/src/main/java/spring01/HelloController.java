8355d0c834ca6dedd79ed0d864c83653f49fea6e
cultist <cultist@cultist-PC>
2014-04-28 09:51:30 +0900

    test

--- WebProgramming2/src/main/java/spring01/HelloController.java
+++ WebProgramming2/src/main/java/spring01/HelloController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class HelloController {
 	@RequestMapping("/hello.do")
-	public ModelAndView hello(){
+	public ModelAndView hello() {
 		ModelAndView result = new ModelAndView("hello");
 		result.addObject("message", "hello, world!");
 		return result;
---
+++ WebProgramming2/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=utf-8"%>
+<html>
+<body>
+	<h2>Hello World!</h2>
+
+	<a href="page1.do">page1</a>
+	<a href="page2.do">page2</a>
+	<a href="page3.do">page3</a>
+
+</body>
+</html>
